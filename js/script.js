b4a84dca7bfce14cfe307323d70f224a6e571663
Alphatih <alphaahmad37@gmail.com>
2023-07-18 14:08:56 +0200

    added a scrollToTop button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//The side menu button 
 const menu = document.getElementById("menu");
 const actions = document.getElementById("actions");
 
@@ -8,3 +9,15 @@ function HandleMenu() {
     menu.classList.toggle("is-active");
     actions.classList.toggle("is-active");
 }
+
+// To top button
+const toTop = document.querySelector(".toTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+
+});
